92114121f88e34f59356514b5c2d6ea1d5a9b24b
Jochen <jo@hassfurter.com>
2019-10-15 14:04:53 +0200

    Angular 'tasks-list'-component revised with Angular class 'Task' (in 'task.model.ts') [1st stage]

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Task} from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -7,9 +8,27 @@ import {Component, OnInit} from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
+    // usual definition as object:
+    // tasks {id: number, name: string, completed: boolean}[] = []
+    // but we use Typescript:
+    tasks: Task[] = [];
+
   constructor() { }
 
   ngOnInit() {
+
+      this.tasks.push( new Task(1, "Aufgabe 1", true, "15/10/2019"));
+      this.tasks.push( new Task(2, "Aufgabe 2", true, "15/10/2019"));
+      this.tasks.push( new Task(3, "Aufgabe 3", true, "15/10/2019"));
   }
 
+  getDueDateLabel(task: Task){
+
+      return task.completed ? 'label-success' : 'label-primary';
+  }
+
+  onTaskChange(event, task) {
+
+      console.log('Task has changed');
+  }
 }
